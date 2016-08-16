5321be06bb44d9c0d3a10877f2ef941a75a08fbf
AlexBradley1990 <Alex.Bradley@live.co.uk>
2016-08-17 00:29:47 +0100

    changes

--- CoBuilder.Interfaces/GUI/ConfigEditorDialog.Designer.cs
+++ CoBuilder.Interfaces/GUI/ConfigEditorDialog.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.SpCForm = new System.Windows.Forms.SplitContainer();
-            this.TrvConfiguration = new System.Windows.Forms.TreeView();
-            this.TrvRoot = new System.Windows.Forms.TreeView();
-            this.CmdConfigAdd = new System.Windows.Forms.Button();
-            this.CmdEdit = new System.Windows.Forms.Button();
             this.CmdRemove = new System.Windows.Forms.Button();
+            this.CmdEdit = new System.Windows.Forms.Button();
+            this.CmdConfigAdd = new System.Windows.Forms.Button();
+            this.TrvConfiguration = new System.Windows.Forms.TreeView();
             this.CmdAddtoConfig = new System.Windows.Forms.Button();
+            this.TrvRoot = new System.Windows.Forms.TreeView();
             ((System.ComponentModel.ISupportInitialize)(this.SpCForm)).BeginInit();
             this.SpCForm.Panel1.SuspendLayout();
             this.SpCForm.Panel2.SuspendLayout();
@@ -58,49 +58,9 @@
             // 
             this.SpCForm.Panel2.Controls.Add(this.CmdAddtoConfig);
             this.SpCForm.Panel2.Controls.Add(this.TrvRoot);
-            this.SpCForm.Size = new System.Drawing.Size(644, 672);
+            this.SpCForm.Size = new System.Drawing.Size(644, 641);
             this.SpCForm.SplitterDistance = 369;
             this.SpCForm.TabIndex = 0;
-            // 
-            // TrvConfiguration
-            // 
-            this.TrvConfiguration.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TrvConfiguration.Location = new System.Drawing.Point(12, 12);
-            this.TrvConfiguration.Name = "TrvConfiguration";
-            this.TrvConfiguration.Size = new System.Drawing.Size(354, 571);
-            this.TrvConfiguration.TabIndex = 0;
-            // 
-            // TrvRoot
-            // 
-            this.TrvRoot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TrvRoot.Location = new System.Drawing.Point(3, 12);
-            this.TrvRoot.Name = "TrvRoot";
-            this.TrvRoot.Size = new System.Drawing.Size(256, 571);
-            this.TrvRoot.TabIndex = 1;
-            // 
-            // CmdConfigAdd
-            // 
-            this.CmdConfigAdd.Location = new System.Drawing.Point(12, 589);
-            this.CmdConfigAdd.Name = "CmdConfigAdd";
-            this.CmdConfigAdd.Size = new System.Drawing.Size(51, 27);
-            this.CmdConfigAdd.TabIndex = 1;
-            this.CmdConfigAdd.Text = "&Add";
-            this.CmdConfigAdd.UseVisualStyleBackColor = true;
-            this.CmdConfigAdd.Click += new System.EventHandler(this.CmdConfigAdd_Click);
-            // 
-            // CmdEdit
-            // 
-            this.CmdEdit.Location = new System.Drawing.Point(69, 589);
-            this.CmdEdit.Name = "CmdEdit";
-            this.CmdEdit.Size = new System.Drawing.Size(51, 27);
-            this.CmdEdit.TabIndex = 2;
-            this.CmdEdit.Text = "&Edit";
-            this.CmdEdit.UseVisualStyleBackColor = true;
-            this.CmdEdit.Click += new System.EventHandler(this.CmdEdit_Click);
             // 
             // CmdRemove
             // 
@@ -112,6 +72,36 @@
             this.CmdRemove.UseVisualStyleBackColor = true;
             this.CmdRemove.Click += new System.EventHandler(this.CmdRemove_Click);
             // 
+            // CmdEdit
+            // 
+            this.CmdEdit.Location = new System.Drawing.Point(69, 589);
+            this.CmdEdit.Name = "CmdEdit";
+            this.CmdEdit.Size = new System.Drawing.Size(51, 27);
+            this.CmdEdit.TabIndex = 2;
+            this.CmdEdit.Text = "&Edit";
+            this.CmdEdit.UseVisualStyleBackColor = true;
+            this.CmdEdit.Click += new System.EventHandler(this.CmdEdit_Click);
+            // 
+            // CmdConfigAdd
+            // 
+            this.CmdConfigAdd.Location = new System.Drawing.Point(12, 589);
+            this.CmdConfigAdd.Name = "CmdConfigAdd";
+            this.CmdConfigAdd.Size = new System.Drawing.Size(51, 27);
+            this.CmdConfigAdd.TabIndex = 1;
+            this.CmdConfigAdd.Text = "&Add";
+            this.CmdConfigAdd.UseVisualStyleBackColor = true;
+            this.CmdConfigAdd.Click += new System.EventHandler(this.CmdConfigAdd_Click);
+            // 
+            // TrvConfiguration
+            // 
+            this.TrvConfiguration.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TrvConfiguration.Location = new System.Drawing.Point(12, 12);
+            this.TrvConfiguration.Name = "TrvConfiguration";
+            this.TrvConfiguration.Size = new System.Drawing.Size(354, 540);
+            this.TrvConfiguration.TabIndex = 0;
+            // 
             // CmdAddtoConfig
             // 
             this.CmdAddtoConfig.Location = new System.Drawing.Point(3, 589);
@@ -122,11 +112,21 @@
             this.CmdAddtoConfig.UseVisualStyleBackColor = true;
             this.CmdAddtoConfig.Click += new System.EventHandler(this.CmdAddtoConfig_Click);
             // 
+            // TrvRoot
+            // 
+            this.TrvRoot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TrvRoot.Location = new System.Drawing.Point(3, 12);
+            this.TrvRoot.Name = "TrvRoot";
+            this.TrvRoot.Size = new System.Drawing.Size(256, 540);
+            this.TrvRoot.TabIndex = 1;
+            // 
             // ConfigEditorDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 672);
+            this.ClientSize = new System.Drawing.Size(644, 641);
             this.Controls.Add(this.SpCForm);
             this.Name = "ConfigEditorDialog";
             this.Text = "ConfigEditorDialog";
--- CoBuilder.Interfaces/GUI/LoginDialog.Designer.cs
+++ CoBuilder.Interfaces/GUI/LoginDialog.Designer.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows.Forms;
-
-namespace CoBuilder.Service.Dialogs
+﻿namespace CoBuilder.Service.GUI
 {
     partial class LoginDialog
     {
@@ -30,27 +28,36 @@ namespace CoBuilder.Service.Dialogs
         /// </summary>
         private void InitializeComponent()
         {
+            this.PbxCoBuilder = new System.Windows.Forms.PictureBox();
             this.grpLogin = new System.Windows.Forms.GroupBox();
             this.CbxRemember = new System.Windows.Forms.CheckBox();
             this.CmbLogin = new System.Windows.Forms.Button();
             this.TxbPassword = new System.Windows.Forms.TextBox();
             this.LblPassword = new System.Windows.Forms.Label();
             this.TxbName = new System.Windows.Forms.TextBox();
-            this.LblUserName = new System.Windows.Forms.Label();
+            this.LblName = new System.Windows.Forms.Label();
             this.GrpWorkplace = new System.Windows.Forms.GroupBox();
             this.CmbApply = new System.Windows.Forms.Button();
             this.CboWorkplace = new System.Windows.Forms.ComboBox();
-            this.LblWorkSpace = new System.Windows.Forms.Label();
-            this.GrpCommands = new System.Windows.Forms.GroupBox();
+            this.GbxCommands = new System.Windows.Forms.GroupBox();
             this.CmbCancel = new System.Windows.Forms.Button();
             this.CmbOK = new System.Windows.Forms.Button();
-            this.PbxCoBuilder = new System.Windows.Forms.PictureBox();
-            this.Lblcobuilder = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxCoBuilder)).BeginInit();
             this.grpLogin.SuspendLayout();
             this.GrpWorkplace.SuspendLayout();
-            this.GrpCommands.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.PbxCoBuilder)).BeginInit();
+            this.GbxCommands.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PbxCoBuilder
+            // 
+            this.PbxCoBuilder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PbxCoBuilder.Image = global::CoBuilder.Service.Properties.Resources.CoBuilder_logo;
+            this.PbxCoBuilder.Location = new System.Drawing.Point(88, 11);
+            this.PbxCoBuilder.Margin = new System.Windows.Forms.Padding(2);
+            this.PbxCoBuilder.Name = "PbxCoBuilder";
+            this.PbxCoBuilder.Size = new System.Drawing.Size(196, 50);
+            this.PbxCoBuilder.TabIndex = 13;
+            this.PbxCoBuilder.TabStop = false;
             // 
             // grpLogin
             // 
@@ -61,20 +68,21 @@ namespace CoBuilder.Service.Dialogs
             this.grpLogin.Controls.Add(this.TxbPassword);
             this.grpLogin.Controls.Add(this.LblPassword);
             this.grpLogin.Controls.Add(this.TxbName);
-            this.grpLogin.Controls.Add(this.LblUserName);
-            this.grpLogin.Location = new System.Drawing.Point(8, 63);
+            this.grpLogin.Controls.Add(this.LblName);
+            this.grpLogin.Location = new System.Drawing.Point(11, 65);
             this.grpLogin.Margin = new System.Windows.Forms.Padding(2);
             this.grpLogin.Name = "grpLogin";
             this.grpLogin.Padding = new System.Windows.Forms.Padding(2);
-            this.grpLogin.Size = new System.Drawing.Size(250, 162);
-            this.grpLogin.TabIndex = 0;
+            this.grpLogin.Size = new System.Drawing.Size(273, 156);
+            this.grpLogin.TabIndex = 14;
             this.grpLogin.TabStop = false;
             this.grpLogin.Text = "Login Authentication";
             // 
             // CbxRemember
             // 
+            this.CbxRemember.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.CbxRemember.AutoSize = true;
-            this.CbxRemember.Location = new System.Drawing.Point(143, 124);
+            this.CbxRemember.Location = new System.Drawing.Point(165, 123);
             this.CbxRemember.Margin = new System.Windows.Forms.Padding(2);
             this.CbxRemember.Name = "CbxRemember";
             this.CbxRemember.Size = new System.Drawing.Size(95, 17);
@@ -99,9 +107,9 @@ namespace CoBuilder.Service.Dialogs
             this.TxbPassword.Location = new System.Drawing.Point(14, 87);
             this.TxbPassword.Margin = new System.Windows.Forms.Padding(2);
             this.TxbPassword.Name = "TxbPassword";
-            this.TxbPassword.Size = new System.Drawing.Size(222, 20);
+            this.TxbPassword.Size = new System.Drawing.Size(246, 20);
             this.TxbPassword.TabIndex = 3;
-            this.TxbPassword.Text = "hKkSVQ2U";
+            this.TxbPassword.Text = "password";
             this.TxbPassword.UseSystemPasswordChar = true;
             // 
             // LblPassword
@@ -121,19 +129,19 @@ namespace CoBuilder.Service.Dialogs
             this.TxbName.Location = new System.Drawing.Point(14, 37);
             this.TxbName.Margin = new System.Windows.Forms.Padding(2);
             this.TxbName.Name = "TxbName";
-            this.TxbName.Size = new System.Drawing.Size(222, 20);
+            this.TxbName.Size = new System.Drawing.Size(246, 20);
             this.TxbName.TabIndex = 1;
             this.TxbName.Text = " cobuilderuk@gmail.com";
             // 
-            // LblUserName
+            // LblName
             // 
-            this.LblUserName.AutoSize = true;
-            this.LblUserName.Location = new System.Drawing.Point(12, 18);
-            this.LblUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.LblUserName.Name = "LblUserName";
-            this.LblUserName.Size = new System.Drawing.Size(35, 13);
-            this.LblUserName.TabIndex = 0;
-            this.LblUserName.Text = "Name";
+            this.LblName.AutoSize = true;
+            this.LblName.Location = new System.Drawing.Point(12, 18);
+            this.LblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(35, 13);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Name";
             // 
             // GrpWorkplace
             // 
@@ -141,23 +149,22 @@ namespace CoBuilder.Service.Dialogs
             | System.Windows.Forms.AnchorStyles.Right)));
             this.GrpWorkplace.Controls.Add(this.CmbApply);
             this.GrpWorkplace.Controls.Add(this.CboWorkplace);
-            this.GrpWorkplace.Controls.Add(this.LblWorkSpace);
-            this.GrpWorkplace.Location = new System.Drawing.Point(8, 230);
+            this.GrpWorkplace.Location = new System.Drawing.Point(11, 225);
             this.GrpWorkplace.Margin = new System.Windows.Forms.Padding(2);
             this.GrpWorkplace.Name = "GrpWorkplace";
             this.GrpWorkplace.Padding = new System.Windows.Forms.Padding(2);
-            this.GrpWorkplace.Size = new System.Drawing.Size(250, 81);
-            this.GrpWorkplace.TabIndex = 1;
+            this.GrpWorkplace.Size = new System.Drawing.Size(273, 65);
+            this.GrpWorkplace.TabIndex = 15;
             this.GrpWorkplace.TabStop = false;
             this.GrpWorkplace.Text = "Select Current Work Place";
             // 
             // CmbApply
             // 
             this.CmbApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.CmbApply.Location = new System.Drawing.Point(181, 32);
+            this.CmbApply.Location = new System.Drawing.Point(204, 24);
             this.CmbApply.Margin = new System.Windows.Forms.Padding(2);
             this.CmbApply.Name = "CmbApply";
-            this.CmbApply.Size = new System.Drawing.Size(56, 37);
+            this.CmbApply.Size = new System.Drawing.Size(56, 27);
             this.CmbApply.TabIndex = 2;
             this.CmbApply.Text = "Apply";
             this.CmbApply.UseVisualStyleBackColor = true;
@@ -167,42 +174,31 @@ namespace CoBuilder.Service.Dialogs
             this.CboWorkplace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.CboWorkplace.FormattingEnabled = true;
-            this.CboWorkplace.Location = new System.Drawing.Point(14, 50);
+            this.CboWorkplace.Location = new System.Drawing.Point(14, 28);
             this.CboWorkplace.Margin = new System.Windows.Forms.Padding(2);
             this.CboWorkplace.Name = "CboWorkplace";
-            this.CboWorkplace.Size = new System.Drawing.Size(150, 21);
+            this.CboWorkplace.Size = new System.Drawing.Size(186, 21);
             this.CboWorkplace.TabIndex = 1;
             // 
-            // LblWorkSpace
+            // GbxCommands
             // 
-            this.LblWorkSpace.AutoSize = true;
-            this.LblWorkSpace.Location = new System.Drawing.Point(14, 25);
-            this.LblWorkSpace.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.LblWorkSpace.Name = "LblWorkSpace";
-            this.LblWorkSpace.Size = new System.Drawing.Size(91, 13);
-            this.LblWorkSpace.TabIndex = 0;
-            this.LblWorkSpace.Text = "coBuilder PRO ID";
-            // 
-            // GrpCommands
-            // 
-            this.GrpCommands.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.GbxCommands.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.GrpCommands.Controls.Add(this.CmbCancel);
-            this.GrpCommands.Controls.Add(this.CmbOK);
-            this.GrpCommands.Location = new System.Drawing.Point(9, 316);
-            this.GrpCommands.Margin = new System.Windows.Forms.Padding(2);
-            this.GrpCommands.Name = "GrpCommands";
-            this.GrpCommands.Padding = new System.Windows.Forms.Padding(2);
-            this.GrpCommands.Size = new System.Drawing.Size(250, 54);
-            this.GrpCommands.TabIndex = 2;
-            this.GrpCommands.TabStop = false;
-            this.GrpCommands.Text = "Commands";
+            this.GbxCommands.Controls.Add(this.CmbCancel);
+            this.GbxCommands.Controls.Add(this.CmbOK);
+            this.GbxCommands.Location = new System.Drawing.Point(11, 293);
+            this.GbxCommands.Margin = new System.Windows.Forms.Padding(2);
+            this.GbxCommands.Name = "GbxCommands";
+            this.GbxCommands.Padding = new System.Windows.Forms.Padding(2);
+            this.GbxCommands.Size = new System.Drawing.Size(273, 54);
+            this.GbxCommands.TabIndex = 16;
+            this.GbxCommands.TabStop = false;
             // 
             // CmbCancel
             // 
             this.CmbCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.CmbCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CmbCancel.Location = new System.Drawing.Point(138, 17);
+            this.CmbCancel.Location = new System.Drawing.Point(183, 17);
             this.CmbCancel.Margin = new System.Windows.Forms.Padding(2);
             this.CmbCancel.Name = "CmbCancel";
             this.CmbCancel.Size = new System.Drawing.Size(77, 24);
@@ -212,9 +208,10 @@ namespace CoBuilder.Service.Dialogs
             // 
             // CmbOK
             // 
+            this.CmbOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.CmbOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.CmbOK.Enabled = false;
-            this.CmbOK.Location = new System.Drawing.Point(31, 17);
+            this.CmbOK.Location = new System.Drawing.Point(107, 17);
             this.CmbOK.Margin = new System.Windows.Forms.Padding(2);
             this.CmbOK.Name = "CmbOK";
             this.CmbOK.Size = new System.Drawing.Size(72, 24);
@@ -222,73 +219,42 @@ namespace CoBuilder.Service.Dialogs
             this.CmbOK.Text = "&OK";
             this.CmbOK.UseVisualStyleBackColor = true;
             // 
-            // PbxCoBuilder
-            // 
-            this.PbxCoBuilder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.PbxCoBuilder.Image = Properties.Resources.CoBuilder_logo;
-            this.PbxCoBuilder.Location = new System.Drawing.Point(62, 6);
-            this.PbxCoBuilder.Margin = new System.Windows.Forms.Padding(2);
-            this.PbxCoBuilder.Name = "PbxCoBuilder";
-            this.PbxCoBuilder.Size = new System.Drawing.Size(196, 50);
-            this.PbxCoBuilder.TabIndex = 12;
-            this.PbxCoBuilder.TabStop = false;
-            // 
-            // Lblcobuilder
-            // 
-            this.Lblcobuilder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Lblcobuilder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(0)))));
-            this.Lblcobuilder.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lblcobuilder.Location = new System.Drawing.Point(8, 376);
-            this.Lblcobuilder.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.Lblcobuilder.Name = "Lblcobuilder";
-            this.Lblcobuilder.Size = new System.Drawing.Size(251, 19);
-            this.Lblcobuilder.TabIndex = 13;
-            this.Lblcobuilder.Text = "Http://www.coBuilder.com";
-            // 
             // LoginDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.CmbCancel;
-            this.ClientSize = new System.Drawing.Size(269, 403);
-            this.Controls.Add(this.Lblcobuilder);
-            this.Controls.Add(this.PbxCoBuilder);
-            this.Controls.Add(this.GrpCommands);
+            this.ClientSize = new System.Drawing.Size(295, 358);
+            this.Controls.Add(this.GbxCommands);
             this.Controls.Add(this.GrpWorkplace);
             this.Controls.Add(this.grpLogin);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(285, 442);
+            this.Controls.Add(this.PbxCoBuilder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "LoginDialog";
-            this.Text = "coBuilder - Login";
+            this.Text = "CoBuilder Login";
+            ((System.ComponentModel.ISupportInitialize)(this.PbxCoBuilder)).EndInit();
             this.grpLogin.ResumeLayout(false);
             this.grpLogin.PerformLayout();
             this.GrpWorkplace.ResumeLayout(false);
-            this.GrpWorkplace.PerformLayout();
-            this.GrpCommands.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.PbxCoBuilder)).EndInit();
+            this.GbxCommands.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private GroupBox grpLogin;
-        private CheckBox CbxRemember;
-        private Button CmbLogin;
-        private TextBox TxbPassword;
-        private Label LblPassword;
-        private TextBox TxbName;
-        private Label LblUserName;
-        private GroupBox GrpWorkplace;
-        private Button CmbApply;
-        private ComboBox CboWorkplace;
-        private Label LblWorkSpace;
-        private GroupBox GrpCommands;
-        private Button CmbCancel;
-        private Button CmbOK;
-        private PictureBox PbxCoBuilder;
-        private Label Lblcobuilder;
+        private System.Windows.Forms.PictureBox PbxCoBuilder;
+        private System.Windows.Forms.GroupBox grpLogin;
+        private System.Windows.Forms.CheckBox CbxRemember;
+        private System.Windows.Forms.Button CmbLogin;
+        private System.Windows.Forms.TextBox TxbPassword;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxbName;
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.GroupBox GrpWorkplace;
+        private System.Windows.Forms.Button CmbApply;
+        private System.Windows.Forms.ComboBox CboWorkplace;
+        private System.Windows.Forms.GroupBox GbxCommands;
+        private System.Windows.Forms.Button CmbCancel;
+        private System.Windows.Forms.Button CmbOK;
     }
 }
